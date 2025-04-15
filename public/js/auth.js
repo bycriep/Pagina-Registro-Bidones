11d95e8cbd310e75ba4a0f556111a05e35280cc7
bycriep <ivanmartinez262935.38@gmail.com>
2025-04-15 18:09:13 +0000

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }
 
             try {
-                const res = await fetch("http://localhost:5000/api/auth/login", {
+                const res = await fetch("/api/auth/login", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ email, password }),
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const rol = "repartidor";
 
             try {
-                const res = await fetch("http://localhost:5000/api/auth/register", {
+                const res = await fetch("/api/auth/register", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ nombre, email, password, rol }),
